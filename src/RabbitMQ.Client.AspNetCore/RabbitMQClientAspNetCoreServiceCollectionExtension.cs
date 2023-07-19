d7fca6d7dd928b8af93170a506a4d7aad6d5c359
zhangxiancheng <365839159@qq.com>
2023-07-19 22:55:21 +0800

    优化

--- src/RabbitMQ.Client.AspNetCore/RabbitMQClientAspNetCoreServiceCollectionExtension.cs
+++ src/RabbitMQ.Client.AspNetCore/RabbitMQClientAspNetCoreServiceCollectionExtension.cs
@@ -1,13 +1,6 @@
 ﻿using Auto.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Polly;
-using RabbitMQ.Client.AspNetCore.RabbitMQ.Client;
-using System;
-using System.Runtime;
-using System.Runtime.CompilerServices;
 
 namespace RabbitMQ.Client.AspNetCore
 {
